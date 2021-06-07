840bd739475bff7c96a7ebc61680464bf9d1b344
Sebastian <sacm1046@gmail.com>
2021-06-07 14:46:28 -0400

    messages response added

--- index.js
+++ index.js
@@ -1,94 +1,85 @@
-const express = require('express');
-const http = require('http');
-const data = require('./db.json');
+const express = require("express");
+const http = require("http");
+const data = require("./db.json");
 const app = express();
-const prefix = '/api';
-const port = process.env.PORT || 4000;
+const prefix = "/api";
+const { PORT } = process.env;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Página de inicio')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Bienvenido a mi primera API</h1>");
+});
 
 app.get(`${prefix}/tasks`, (req, res) => {
-    if (data.length > 0) {
-        res.status(200).json({
-            'method': 'GET',
-            'data': data
-        })
-    } else {
-        res.status(404).json({
-            'error': "Not found"
-        })
-    }
+  if (data.length > 0) {
+    res.status(200).json(data);
+  } else {
+    res.status(404).json({
+      error: "No hay tareas registradas",
+    });
+  }
 });
 
 app.get(`${prefix}/task/:id`, (req, res) => {
-    const { params } = req;
-    const taskFound = data.filter(item => item.id === parseInt(params.id))
-    if (taskFound.length > 0) {
-        res.status(200).json({
-            'method': 'GET',
-            'data': taskFound[0]
-        })
-    } else {
-        res.status(404).json({
-            'error': "Not found"
-        })
-    }
+  const { params } = req;
+  const taskFound = data.filter((item) => item.id === parseInt(params.id));
+  if (taskFound.length > 0) {
+    res.status(200).json(taskFound[0]);
+  } else {
+    res.status(404).json({
+      error: "Tarea no encontrada",
+    });
+  }
 });
 
 app.post(`${prefix}/task`, (req, res) => {
-    const { body } = req;
-    const idCheck = data.some(item => item.id === body.id)
-    if (idCheck) {
-        res.status(500).json({
-            'error': 'Id is already crated'
-        })
-    } else {
-        data.push(body)
-        res.status(201).json({
-            'method': 'POST',
-            'success': 'Task created successfully'
-        })
-    }
+  const { body } = req;
+  const idCheck = data.some((item) => item.id === body.id);
+  if (idCheck) {
+    res.status(500).json({
+      error: "EL ID ya se encuentra registrado",
+    });
+  } else {
+    data.push(body);
+    res.status(201).json({
+      success: "Tarea creada con éxito",
+    });
+  }
 });
 
 app.patch(`${prefix}/task/:id`, (req, res) => {
-    const { body, params } = req
-    const index = data.findIndex(item => item.id === parseInt(params.id))
-    if (index < 0) {
-        res.status(404).json({
-            'error': 'Not found'
-        })
-    } else {
-        data[index].description = body.description
-        data[index].responsable = body.responsable
-        res.status(201).json({
-            'method': 'PATCH',
-            'success': 'Task updated successfully'
-        })
-    }
+  const { body, params } = req;
+  const index = data.findIndex((item) => item.id === parseInt(params.id));
+  if (index < 0) {
+    res.status(404).json({
+      error: "Tarea no encontrada",
+    });
+  } else {
+    data[index].description = body.description;
+    data[index].responsable = body.responsable;
+    res.status(201).json({
+      success: "Tarea actualizada con éxito",
+    });
+  }
 });
 
 app.delete(`${prefix}/task/:id`, (req, res) => {
-    const { params } = req;
-    const index = data.findIndex(item => item.id === parseInt(params.id))
-    if (index < 0) {
-        res.status(404).json({
-            'error': 'Not found'
-        })
-    } else {
-        data.splice(index, 1)
-        res.status(200).json({
-            'method': 'DELETE',
-            'success': 'Task deleted successfully'
-        })
-    }
+  const { params } = req;
+  const index = data.findIndex((item) => item.id === parseInt(params.id));
+  if (index < 0) {
+    res.status(404).json({
+      error: "Tarea no encontrada",
+    });
+  } else {
+    data.splice(index, 1);
+    res.status(200).json({
+      success: "Tarea borrada con éxito",
+    });
+  }
 });
 
 const server = http.createServer(app);
-server.listen(port, () => {
-    console.log(`Server on port ${port}`)
+server.listen(PORT || 4000, () => {
+  console.log(`Server on port ${PORT || 4000}`);
 });
